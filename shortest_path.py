d378ee2447af75a3cac169887e8824203bb82938
unknown <jakeandersonbell@gmail.com>
2019-12-12 11:57:06 +0000

    Basic psuedo code for file structure

---
+++ shortest_path.py
@@ -0,0 +1,17 @@
+"""Task 4: Shortest Path"""
+
+# import libraries
+
+# make some functions that can colour network paths
+
+# open the itn json and access the 'roadlinks' layer
+
+# initialise a MultiDiGraph object
+
+# make uphill directed edges adhere to Naismith's rule
+#   Naismith's rule will be implemented as a ratio between vertical and horizontal travel weight
+# Make downhill edges just have Naismith's weight
+
+# do dijkstra path method on user location and high point
+
+# Display the path
