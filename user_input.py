65fa9c3b22148be3041bb794e570bf616fa06bd8
ucesmhg <maike.gatzlaff.19@ucl.ac.uk>
2019-12-17 13:06:30 +0100

    feat: pseudo code user input and location check

---
+++ user_input.py
@@ -0,0 +1,16 @@
+"""Task 1: User Input"""
+
+## load required packages
+# import shapely
+# from shapely.geometry import Point
+
+## storing box extent
+
+
+
+## getting user input
+# ask user for location in BNG
+# save user input
+
+## test if user is within box extent
+# stop application if user is outside box extent
