2b10a8fbfd4bdc5434f8845263619f4536c080c3
ucesmhg <maike.gatzlaff.19@ucl.ac.uk>
2019-12-17 21:25:50 +0100

    feat: basic steps for nearest integrated transport network

--- highest_point_identification.py
+++ highest_point_identification.py
@@ -1,15 +1,26 @@
 """Task 2: Highest Point Identification"""
 
-## import packages
-# import rasterio
-# from rasterio import plot
+## import required packages
+import rasterio
+from rasterio import plot
+
+import shapely
+from shapely.geometry import Point
+
+import numpy as np
+
+import pyproj
 
 ## store data
-# elevation = rasterio.open("SZ.asc")
+elevation = rasterio.open('sz.asc')
 
 ## create 5km buffer around user location
-
 # test user location
+user_location_easting = 450000
+user_location_northing = 85000
+user_location = Point(user_location_easting, user_location_northing)
+
+
 
 # generate a rasterio 5km window based on user location
 
@@ -19,5 +30,5 @@
 
 
 ## identify highest point
-
-# find and localise max z value
+elevation_np = np.loadtxt("SZ.asc", skiprows=6) # replace with buffer
+highest_point = np.amax(elevation_np) # find and localise max z value
