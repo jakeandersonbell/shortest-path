71535aed43249bd368de30aa4958fbdcf956b1f2
ucesmhg <maike.gatzlaff.19@ucl.ac.uk>
2019-12-17 13:35:30 +0100

    feat: basic steps for highest point identification

---
+++ highest_point_identification.py
@@ -0,0 +1,23 @@
+"""Task 2: Highest Point Identification"""
+
+## import packages
+# import rasterio
+# from rasterio import plot
+
+## store data
+# elevation = rasterio.open("SZ.asc")
+
+## create 5km buffer around user location
+
+# test user location
+
+# generate a rasterio 5km window based on user location
+
+# create a rasterised 5km buffer
+
+## clip elevation array to 5km buffer
+
+
+## identify highest point
+
+# find and localise max z value
