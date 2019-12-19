1cc04e4b618a20687cba782b1949ec20ce4e13e1
ucesmhg <maike.gatzlaff.19@ucl.ac.uk>
2019-12-19 16:32:05 +0100

    feat: check if user is on isle of wight based on shapefile

--- user_input.py
+++ user_input.py
@@ -2,8 +2,9 @@
 
 import shapely
 from shapely.geometry import Point
+import geopandas as gpd
 
-#storing box extent
+# storing box extent
 min_easting = 430000
 min_northing = 80000
 max_easting = 465000
@@ -16,10 +17,20 @@ user_location_northing = float(input("Insert Northing (y coordinate) of your loc
 
 user_location = Point(user_location_easting, user_location_northing)
 
-# test if user is within box extent
+# test if user is within box extent aka not closer than 5km to the raster edge
 if min_easting <= user_location_easting <= max_easting and min_northing <= user_location_northing <= max_northing:
     location = "inside"
 else:
     location = "outside"
     print("You are too close to the edge - cannot calculate quickest route to the highest point")
-    exit() # stop application if user is outside box extent
+    exit()  # stop application if user is outside box extent
+
+# test if user is on the isle of wight and not in the sea
+island = gpd.GeoDataFrame.from_file('isle_of_wight.shp')
+
+if (island.contains(user_location)).bool():
+    location = "on land"
+else:
+    location = "in the sea"
+    print ("you are not on the island")
+    exit()  # stop application if user is outside box extent
