c46ca6215aad2eb9179e34eb6b076edcb311664e
ucesmhg <maike.gatzlaff.19@ucl.ac.uk>
2019-12-20 01:01:57 +0100

    feat: generate rasterio 5km window around user location

--- highest_point_identification.py
+++ highest_point_identification.py
@@ -2,10 +2,8 @@
 
 ## import required packages
 import rasterio
-from rasterio import plot
-
-import shapely
-from shapely.geometry import Point
+from rasterio.windows import Window
+from matplotlib import pyplot
 
 import numpy as np
 
@@ -15,14 +13,34 @@ import pyproj
 elevation = rasterio.open('sz.asc')
 
 ## create 5km buffer around user location
-# test user location
-user_location_easting = 450000
-user_location_northing = 85000
-user_location = Point(user_location_easting, user_location_northing)
-
-
+# test user location and box extents from task 1
+user_location_easting = 449825 # coords for testing, close to centroid of isle of wight shapefile
+user_location_northing = 86492
+min_easting = 430000
+min_northing = 80000
+max_easting = 465000
+max_northing = 95000
 
 # generate a rasterio 5km window based on user location
+# basic code from https://rasterio.readthedocs.io/en/stable/topics/reading.html
+# basic code from https://rasterio.readthedocs.io/en/stable/topics/windowed-rw.html#windowrw
+
+user_easting_cell = round(((user_location_easting - (min_easting - 5000)) / 5) - 500)  # user location x coord transformed to asc file row off cell location
+user_northing_cell = round(((user_location_northing - (min_northing - 5000)) / 5) - 500)
+col_off = user_easting_cell - 500  # 2.5km west of user location to generate 5km window around user location
+row_off = user_northing_cell - 500
+if col_off < 1:  # to ensure that col_off is within elevation raster, hence search radius might be less than 5km
+    col_off = 1
+if row_off < 1:
+    row_off = 1
+
+width = 1000  # cellsize = 5m, window 5km around user point
+height = 1000
+with rasterio.open('SZ.asc') as src:
+    user_window = src.read(1, window=Window(col_off, row_off, width, height))
+
+pyplot.imshow(user_window, cmap='pink')
+#pyplot.show()
 
 # create a rasterised 5km buffer
 
