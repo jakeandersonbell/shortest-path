e52792b243c1347c4152875ac6fccead7bd25069
ucesmhg <maike.gatzlaff.19@ucl.ac.uk>
2019-12-20 01:19:21 +0100

    fix: length of window edges must be 10km/ 2000 cells as 5km should be in all directions from the user's location

--- highest_point_identification.py
+++ highest_point_identification.py
@@ -27,20 +27,21 @@ max_northing = 95000
 
 user_easting_cell = round(((user_location_easting - (min_easting - 5000)) / 5) - 500)  # user location x coord transformed to asc file row off cell location
 user_northing_cell = round(((user_location_northing - (min_northing - 5000)) / 5) - 500)
-col_off = user_easting_cell - 500  # 2.5km west of user location to generate 5km window around user location
-row_off = user_northing_cell - 500
+col_off = user_easting_cell - 1000 # 5km west of user location to generate 5km window around user location
+row_off = user_northing_cell - 1000 # 5km south of user location
 if col_off < 1:  # to ensure that col_off is within elevation raster, hence search radius might be less than 5km
     col_off = 1
 if row_off < 1:
     row_off = 1
 
-width = 1000  # cellsize = 5m, window 5km around user point
-height = 1000
+width = 2000 # cellsize = 5m, user location in centre of window, therefore 5km into each direction -> 10km from col/row_off
+height = 2000
+
 with rasterio.open('SZ.asc') as src:
     user_window = src.read(1, window=Window(col_off, row_off, width, height))
 
 pyplot.imshow(user_window, cmap='pink')
-#pyplot.show()
+# pyplot.show()
 
 # create a rasterised 5km buffer
 
