47f9a023aa4d439a449cb6b0794c97d88ddbb17b
unknown <jakeandersonbell@gmail.com>
2019-12-23 13:32:07 +0000

    Naismith's rule working between specified nodes

--- shortest_path.py
+++ shortest_path.py
@@ -33,10 +33,6 @@ with open('data/itn/solent_itn.json', 'r') as f:
 # initialise a MultiDiGraph object
 g = nx.MultiDiGraph()
 
-# This gets the height at a given point
-# coords = road_links[next(iter(road_links))]['coords'][0]
-# list(dataset.sample([(coords[0], coords[1])], 1))
-
 # make uphill directed edges adhere to Naismith's rule
 #   Naismith's rule will be implemented as a ratio between vertical and horizontal travel weight
 # Make downhill edges just have length weight
@@ -49,10 +45,10 @@ for i, link in enumerate(road_links):
     next_elev = list(dataset.sample([(next_coords[0], next_coords[1])], 1))[0][0]
     elev_diff = next_elev - prev_elev  # Change in elevation
     forward, backward = 0, 0  # The default for no change in elevation
-    # if elev_diff > 0:  # Going uphill
-    #     forward = elev_diff * 8.333  # The vert/horiz ratio multiplier
-    # elif elev_diff < 0:  # Downhill
-    #     backward = (elev_diff * -1) * 8.333
+    if elev_diff > 0:  # Going uphill
+        forward = elev_diff * 8.333  # The vert/horiz ratio multiplier
+    elif elev_diff < 0:  # Downhill
+        backward = (elev_diff * -1) * 8.333
     g.add_edge(road_links[link]['start'], road_links[link]['end'], fid=link,
                weight=(road_links[link]['length'] + forward))
     g.add_edge(road_links[link]['end'], road_links[link]['start'], fid=link,
