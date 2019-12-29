547b8e1af4300e20985f677fcfeabbab877ee4e9
unknown <jakeandersonbell@gmail.com>
2019-12-29 12:23:21 +0000

    Refactorised user_input to have functions used by main.py

---
+++ main.py
@@ -0,0 +1,76 @@
+
+"""Flood Emergency Planning"""
+
+from user_input import *
+
+"""Task 1: User Input"""
+
+user_location = user_input()  #
+iow_extent, iow_5k_extent = get_ext_poly()
+if check_extent(user_location, iow_extent, iow_5k_extent):
+    # We don't need to extend the region
+    pass
+
+if on_land(user_location):
+    # The user is on land
+    pass
+
+
+"""Task 2: Highest Point Identification"""
+
+
+# Use window function to limit size of raster around the buffered point
+
+# Get the elevation band
+# elevation = dataset.read(1)
+
+# Somehow get the location of the highest point in the window
+# numpy.amax(elevation)  # gets the highest point
+# res = numpy.where(elevation == numpy.amax(elevation))  # gets the array index
+
+# Translate the numpy array position into a geo coord
+# dataset.xy(res[0][0], res[1][0])
+
+"""Task 3: Nearest Integrated Transport Network"""
+
+# Read in the ITN nodes
+# nodes = gpd.read_file('data/roads/nodes.shp')
+
+# create and index, bounding box
+# idx = index.Index()
+# br = dataset.bounds[0], dataset.bounds[1], dataset.bounds[2], dataset.bounds[3]
+# idx.insert(0, br)  # don't do this
+
+# loop though points in nodes and insert - I think this works
+# for i, j in enumerate(nodes['geometry'].iteritems()):  # Syntactic sugar
+#     pbr = j[1].xy[0][0], j[1].xy[1][0], j[1].xy[0][0], j[1].xy[1][0]
+#     idx.insert(i, pbr)
+
+# for i in idx.nearest((450000,87000), num_result=1, objects=True):
+#     print(i)
+
+# Identify the nearest ITN node to input point - r-trees
+
+"""Task 4: Shortest Path"""
+
+# import libraries
+
+# make some functions that can colour network paths
+
+# open the itn json and access the 'roadlinks' layer
+
+# initialise a MultiDiGraph object
+
+# make uphill directed edges adhere to Naismith's rule
+#   Naismith's rule will be implemented as a ratio between vertical and horizontal travel weight
+# Make downhill edges just have Naismith's weight
+
+# do dijkstra path method on user location and high point
+
+
+"""Task 5: Map Plotting"""
+
+"""Task 6: Extend the Region"""
+
+# Buffer the original raster?
+
--- user_input.py
+++ user_input.py
@@ -1,38 +1,58 @@
 """Task 1: User Input"""
 
-import shapely
+from shapely.geometry import Point, Polygon
 from shapely.geometry import Point
 import geopandas as gpd
 
-# storing box extent
-min_easting = 430000
-min_northing = 80000
-max_easting = 465000
-max_northing = 95000
 
-# getting user input
-print('Please provide your current location in BNG coordinates')
-user_location_easting = float(input('Insert Easting (x coordinate) of your location: '))
-user_location_northing = float(input('Insert Northing (y coordinate) of your location: '))
+def get_ext_poly(bb=(430000, 80000, 465000, 95000)):
+    radius = 5000
+    iow_extent = Polygon([[bb[0], bb[1]], [bb[0], bb[3]], [bb[3], bb[4]], [bb[3], bb[1]]])
+    iow_5k_extent = iow_extent.buffer(-radius)
+    return [iow_extent, iow_5k_extent]
 
-user_location = Point(user_location_easting, user_location_northing)
+
+def user_input():
+    inp = input("Press ENTER to begin\n")
+    print("Please provide your current location in BNG coordinates\n\nPress 'q' at any time to quit\n")
+    while inp.lower() != 'q':
+        inp = input("Please enter a space-separated coordinate pair: ")
+        if inp.lower() != 'q':
+            if len(inp.split()) != 2:
+                print("You did not enter a correctly formatted coordinate pair!\nPlease try again")
+            else:
+                for i in [i for i in inp if i not in "1234567890 "]:
+                    inp = inp.replace(i, "")
+                if len(inp.split()[0]) != 6 or len(inp.split()[1]) != 6:
+                    print("Each coordinate must contain 6 digits\nPlease try again\n"
+                          "\x1B[3mTip: If one of your coordinates has 5 digits, try preceding it with 0\x1B[23m")
+                else:
+                    coords = [int(i) for i in inp.split()]
+                    return Point(coords)
+
 
 # test if user is within box extent aka not closer than 5km to the raster edge
-if min_easting <= user_location_easting <= max_easting and min_northing <= user_location_northing <= max_northing:
-    location = 'inside'
-else:
-    location = 'outside'
-    print('You are too close to the edge - cannot calculate quickest route to the highest point')
-    exit()  # stop application if user is outside box extent
+def check_extent(user, extent, extent_5k):
+    if extent.contains(user):
+        inside = True
+    else:
+        inside = True
+        print('You are too close to the edge - cannot calculate quickest route to the highest point')
+        exit()  # stop application if user is outside box extent
+    if extent_5k.contains(user):
+        extend = True
+    return extend
+
 
 # test if user is on the isle of wight and not in the sea
 # adjusted from https://automating-gis-processes.github.io/CSC18/lessons/L4/point-in-polygon.html
 
-island = gpd.GeoDataFrame.from_file('isle_of_wight.shp')
-
-if (island.contains(user_location)).bool():
-    location = 'on land'
-else:
-    location = 'in the sea'
-    print ('you are not on the island')
-    exit()  # stop application if user is outside box extent
+def on_land(user):
+    island = gpd.GeoDataFrame.from_file('isle_of_wight.shp')
+    if (island.contains(user)).bool():
+        land = True
+    else:
+        land = False
+        print('you are not on the island')
+        exit()  # stop application if user is outside box extent
+    return land
