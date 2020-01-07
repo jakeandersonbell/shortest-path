a7353a16237ec258761a1f008ed786ef0145fe07
jakeandersonbell <jakeandersonbell@gmail.com>
2020-01-07 13:05:49 +0000

    Additional flooded feature added and working

--- nearest_itn.py
+++ nearest_itn.py
@@ -10,15 +10,16 @@ def nearest_itn(target_location, idx):
     Returns list(object name, Point(node location)).
     """
     obj = list(idx.nearest((target_location.xy[0][0], target_location.xy[1][0]), 1, objects=True))[0]
-    start = obj.object
+    start_name = obj.object
     start_p = Point((obj.bounds[0], obj.bounds[2]))
-    print("The nearest itn node to " + str(target_location.bounds[0:2]) + " is: " +
-          str(start) + " at: " + str(start_p.bounds[0:2]))
-    return [start, start_p]
+    print("\nThe nearest itn node to " + str(target_location.bounds[0:2]) + " is " +
+          str(start_name) + " at " + str(start_p.bounds[0:2]))
+    return [start_name, start_p]
 
 
 def make_index(node_shape_path='data/roads/nodes.shp'):
     """Reads in a shp. of nodes and returns an rtree index"""
+    print("\nMaking index...")
     nodes = gpd.read_file(node_shape_path)
 
     idx = index.Index()
